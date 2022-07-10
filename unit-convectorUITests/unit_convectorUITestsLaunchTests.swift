6bd7ffe16f59077aa150c5c9a7d75f363b9775a0
Ivan Hedz <ivan.hedz@binary-studio.com>
2022-07-10 18:57:11 +0300

    Initial Commit

---
+++ unit-convectorUITests/unit_convectorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  unit_convectorUITestsLaunchTests.swift
+//  unit-convectorUITests
+//
+//  Created by Ivan Hedz on 10.07.2022.
+//
+
+import XCTest
+
+class unit_convectorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
